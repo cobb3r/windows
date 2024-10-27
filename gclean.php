7c7a1b6a77ce5e5ba46cc03e6851708142a1ae1a
cobb3r <182259162+cobb3r@users.noreply.github.com>
2024-10-27 22:27:35 +0000

    Create gclean.php

---
+++ gclean.php
@@ -0,0 +1,134 @@
+<html>
+    <head>
+        <title>Gutter Cleaning</title>
+        <meta name="description" content="Exterior Property Renovation Specialists Covering All of Birmingham.">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+        <script src="https://kit.fontawesome.com/b15a8855ee.js" crossorigin="anonymous"></script>
+        <script src="../Windows/windows.js"></script>
+        <link rel="stylesheet" href="../Windows/windows.css?php echo time(); ?>">
+    </head>
+    <body class="container-fluid p-0">
+        <div id="loading">
+            <img src="../Images/loading.gif" alt="">
+        </div>
+        <header class="row m-0 sticky-top">
+            <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #62abf3;">
+                <a class="navbar-brand ms-2" href="../Windows/windows.php">Sparkling Windows</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navigation">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="../Windows/windows.php">Home</a>
+                        </li>
+                        <li class="nav-item">   
+                            <a class="nav-link" href="../Windows/about.php">About Us</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" data-toggle="dropdown">Services</a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="../Windows/wclean.php">Window Cleaning</a>
+                                <a class="dropdown-item" href="../Windows/gclean.php">Gutter Cleaning</a>
+                                <a class="dropdown-item" href="../Windows/patio.php">Patio Cleaning</a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="../Windows/price.php">Get A Quote</a>
+                            </div>
+                        </li>
+                    </ul>
+                    <form class="form-inline ms-auto my-lg-0">
+                        <div class="input-group">
+                            <input class="form-control me-sm-2" type="search" placeholder="Search" >
+                            <button id="searchBut" class="btn btn-outline-success me-2 my-sm-0" type="submit">Search</button>
+                        </div>
+                    </form>
+                </div>
+            </nav>
+        </header>
+        <main class="row m-0">
+            <div class="col-12 p-0" style="width: 100%;">
+                <div class="row p-0 m-0" style="width: 100%;">
+                    <div class="position-absolute" style="transform: translate(10%, 10vh); max-width:80vw; z-index: 999;">
+                        <h1>Gutter Cleaning</h1>
+                        <button id="quote">Get A Quote</button>
+                    </div>
+                    <video id="backVd" class="p-0" src="../Images/Site Ken Burns.mov" autoplay muted></video>
+                </div>
+                <div class="row">
+                    <div class="col-12 text-center">
+                        <h1>What We Can Offer</h1>
+                        <p>blahblahblah blahblahblah</p>
+                    </div>
+                </div>
+                <div class="row my-1">
+                    <div class="col-sm-12 col-lg-8 d-flex justify-content-center align-items-center ps-3">
+                        <p>Windows</p>
+                    </div>
+                    <div class="vidcon col-sm-12 col-lg-3 d-flex align-items-center justify-content-center" style="overflow: hidden;">
+                        <img id="intrimg" src="../Images/laxey.jpg" style="max-height: 100%; max-width: 100%;">
+                    </div>
+                </div>
+                <div class="row my-1">
+                    <div class="col-1"></div>
+                    <div class="vidcon order-2 order-lg-1 col-sm-12 col-lg-3 d-flex align-items-center justify-content-center" style="overflow: hidden;">
+                        <img id="intrimg" src="../Images/laxey.jpg" style="max-height: 100%;max-width:100%;">
+                    </div>
+                    <div class="order-1 order-lg-2 col-lg-8 d-flex justify-content-center align-items-center">
+                        <p>Conservatories</p>
+                    </div>
+                </div>
+            </div>
+        </main>
+        <footer class="row d-flex justify-content-center align-items-center">
+            <div class="col-7 col-md-3" style="overflow: hidden;">
+                <img class="ms-2" src="../Images/aurelia.jpg" alt="" style="width: 100%; height:40vh; border-radius:1em">
+            </div>
+            <div class="col-12 col-sm-6 col-md-5 text-center order-2 order-md-1">
+                <ul class="p-0">
+                    <li>
+                        <a href="../Windows/windows.php">Home</a>
+                    </li>
+                    </br>
+                    <li>
+                        <a href="../Windows/about.php">About Us</a>
+                    </li>
+                    </br>
+                    <li>
+                        <a href="../Windows/wclean.php">Window Cleaning</a>
+                    </li>
+                    </br>
+                    <li>
+                        <a href="../Windows/gclean.php">Gutter Cleaning</a>
+                    </li>
+                    </br>
+                    <li>
+                        <a href="../Windows/patio.php">Patio Cleaning</a>
+                    </li>
+                    </br>
+                    <li>
+                        <a href="../Windows/price.php">Get A Quote</a>
+                    </li>
+                    <hr>
+                </ul>
+            </div>
+            <div class="col-12 col-sm-6 col-md-4 text-center order-1 order-md-2">
+                <h3>Sparkling Windows</h2>
+                </br>
+                <p>33 All Saints Drive</p>
+                <p>Birmingham</p>
+                <p>United Kingdom</p>
+                <p>B74 4AG</p>
+                </br>
+                <p><a href="tel:+447624315231">Phone</a></p>
+                <p><a href="mailto:tylerannis55@gmail.com">Email</a></p>
+                <hr>
+            </div>
+        </footer>
+    </body>
+</html>
