8973f39f28876003feb9e57bdd9d16fb7a6690fb
cobb3r <182259162+cobb3r@users.noreply.github.com>
2024-10-27 22:26:04 +0000

    Create wclean.php

---
+++ wclean.php
@@ -0,0 +1,134 @@
+<html>
+    <head>
+        <title>Window Cleaning</title>
+        <meta name="description" content="Exterior Property Renovation Specialists Covering All of Birmingham.">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+        <script src="https://kit.fontawesome.com/b15a8855ee.js" crossorigin="anonymous"></script>
+        <script src="../Windows/windows.js"></script>
+        <link rel="stylesheet" href="../Windows/windows.css?php echo time(); ?>">
+    </head>
+    <body class="container-fluid p-0">
+        <div id="loading">
+            <img src="../Images/loading.gif" alt="">
+        </div>
+        <header class="row m-0 sticky-top">
+            <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #62abf3;">
+                <a class="navbar-brand ms-2" href="../Windows/windows.php">Sparkling Windows</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navigation">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="../Windows/windows.php">Home</a>
+                        </li>
+                        <li class="nav-item">   
+                            <a class="nav-link" href="../Windows/about.php">About Us</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" data-toggle="dropdown">Services</a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="../Windows/wclean.php">Window Cleaning</a>
+                                <a class="dropdown-item" href="../Windows/gclean.php">Gutter Cleaning</a>
+                                <a class="dropdown-item" href="../Windows/patio.php">Patio Cleaning</a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="../Windows/price.php">Get A Quote</a>
+                            </div>
+                        </li>
+                    </ul>
+                    <form class="form-inline ms-auto my-lg-0">
+                        <div class="input-group">
+                            <input class="form-control me-sm-2" type="search" placeholder="Search" >
+                            <button id="searchBut" class="btn btn-outline-success me-2 my-sm-0" type="submit">Search</button>
+                        </div>
+                    </form>
+                </div>
+            </nav>
+        </header>
+        <main class="row m-0">
+            <div class="col-12 p-0" style="width: 100%;">
+                <div class="row p-0 m-0" style="width: 100%;">
+                    <div class="position-absolute" style="transform: translate(10%, 10vh); max-width:80vw; z-index: 999;">
+                        <h1>Window Cleaning</h1>
+                        <button id="quote">Get A Quote</button>
+                    </div>
+                    <video id="backVd" class="p-0" src="../Images/Site Ken Burns.mov" autoplay muted></video>
+                </div>
+                <div class="row">
+                    <div class="col-12 text-center">
+                        <h1>What We Can Offer</h1>
+                        <p>Our window cleaning business is dedicated to providing top-notch, reliable, and comprehensive services for both residential and commercial clients, ensuring every window is cleaned to perfection. We utilize cutting-edge equipment, such as water-fed poles with purified water systems and traditional hand-cleaning techniques, to deliver spotless, streak-free results. Whether it’s a small home or a multi-story office building, we handle windows of all sizes and heights with precision and care. Safety is at the forefront of our operations, especially when it comes to high-rise or hard-to-reach windows, so our team of trained technicians follows strict safety protocols and uses advanced access equipment, such as ladders, harnesses, and scaffolding, as needed. Our services are designed to be flexible and convenient for our clients. We offer customized cleaning schedules, from one-time deep cleans to routine maintenance plans, ensuring that windows remain consistently clear and well-maintained throughout the year. We also understand the importance of minimizing disruptions, especially for businesses, so we work efficiently and discreetly to complete our tasks without affecting daily operations. Our eco-friendly cleaning solutions are gentle on the environment but tough on grime, leaving windows crystal clear without harmful chemicals. Customer satisfaction is at the heart of our business. We take the time to understand our clients’ specific needs and go the extra mile to ensure those needs are met. From initial consultation to post-service follow-up, we maintain open communication and offer a guarantee on our work to give clients peace of mind. Our mission is not just to clean windows but to enhance the overall appearance and atmosphere of every property we serve, bringing light and clarity to any space.</p>
+                    </div>
+                </div>
+                <div class="row my-1">
+                    <div class="col-sm-12 col-lg-8 d-flex justify-content-center align-items-center ps-3">
+                        <p>Our window cleaning process is designed to deliver a spotless, streak-free finish every time, using advanced tools and eco-friendly solutions. Whether for residential or commercial properties, we focus on efficiency, safety, and attention to detail to ensure the best results. Here's how we clean windows in three key steps: Preparation and Assessment: We start by inspecting the windows and surrounding areas, removing dust, debris, and cobwebs from the frames, sills, and screens to prevent smearing during the cleaning process. Cleaning and Scrubbing: Using eco-friendly cleaning solutions, we thoroughly scrub the glass to remove dirt, grime, and stains. For higher windows, we utilize water-fed poles with purified water to ensure a deep clean without streaks or mineral deposits. Squeegee and Detailing: Our technicians expertly use professional-grade squeegees to remove the cleaning solution in smooth, overlapping strokes, leaving the windows clear and streak-free. We finish by drying the edges and polishing frames for a spotless shine.</p>
+                    </div>
+                    <div class="vidcon col-sm-12 col-lg-3 d-flex align-items-center justify-content-center" style="overflow: hidden;">
+                        <img id="intrimg" src="../Images/laxey.jpg" style="max-height: 100%; max-width: 100%;">
+                    </div>
+                </div>
+                <div class="row my-1">
+                    <div class="col-1"></div>
+                    <div class="vidcon order-2 order-lg-1 col-sm-12 col-lg-3 d-flex align-items-center justify-content-center" style="overflow: hidden;">
+                        <img id="intrimg" src="../Images/laxey.jpg" style="max-height: 100%;max-width:100%;">
+                    </div>
+                    <div class="order-1 order-lg-2 col-lg-8 d-flex justify-content-center align-items-center">
+                        <p>Our conservatory cleaning process focuses on restoring the beauty and clarity of your space while ensuring that every surface is meticulously cleaned and maintained. Using specialized techniques and eco-friendly products, we aim to enhance the overall appearance and longevity of your conservatory. Heres how we achieve exceptional results in three key steps: Initial Assessment and Preparation: We begin by inspecting the conservatory to identify areas that require special attention, such as stubborn stains or built-up grime. We remove furniture or items as necessary and protect surrounding areas to prevent any damage during the cleaning process. Thorough Cleaning: Our team uses high-quality cleaning solutions and tools to scrub the glass panels, frames, and roof, effectively removing dirt, algae, and mildew. For hard-to-reach areas, we employ extendable cleaning poles and pressure washing techniques, ensuring a deep clean without harming the structure. Final Inspection and Detailing: After washing all surfaces, we perform a thorough inspection to ensure everything is spotless and free of streaks. We pay special attention to the edges, seals, and fittings, polishing them to achieve a gleaming finish that enhances the beauty of your conservatory.</p>
+                    </div>
+                </div>
+            </div>
+        </main>
+        <footer class="row d-flex justify-content-center align-items-center">
+            <div class="col-7 col-md-3" style="overflow: hidden;">
+                <img class="ms-2" src="../Images/aurelia.jpg" alt="" style="width: 100%; height:40vh; border-radius:1em">
+            </div>
+            <div class="col-12 col-sm-6 col-md-5 text-center order-2 order-md-1">
+                <ul class="p-0">
+                    <li>
+                        <a href="../Windows/windows.php">Home</a>
+                    </li>
+                    </br>
+                    <li>
+                        <a href="../Windows/about.php">About Us</a>
+                    </li>
+                    </br>
+                    <li>
+                        <a href="../Windows/wclean.php">Window Cleaning</a>
+                    </li>
+                    </br>
+                    <li>
+                        <a href="../Windows/gclean.php">Gutter Cleaning</a>
+                    </li>
+                    </br>
+                    <li>
+                        <a href="../Windows/patio.php">Patio Cleaning</a>
+                    </li>
+                    </br>
+                    <li>
+                        <a href="../Windows/price.php">Get A Quote</a>
+                    </li>
+                    <hr>
+                </ul>
+            </div>
+            <div class="col-12 col-sm-6 col-md-4 text-center order-1 order-md-2">
+                <h3>Sparkling Windows</h2>
+                </br>
+                <p>33 All Saints Drive</p>
+                <p>Birmingham</p>
+                <p>United Kingdom</p>
+                <p>B74 4AG</p>
+                </br>
+                <p><a href="tel:+447624315231">Phone</a></p>
+                <p><a href="mailto:tylerannis55@gmail.com">Email</a></p>
+                <hr>
+            </div>
+        </footer>
+    </body>
+</html>
