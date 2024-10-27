828985edd0825a3020286589ae0e23b6e87ce4d7
cobb3r <182259162+cobb3r@users.noreply.github.com>
2024-10-27 22:23:17 +0000

    Create about.php

---
+++ about.php
@@ -0,0 +1,145 @@
+<html>
+    <head>
+        <title>About Us</title>
+        <meta name="description" content="Exterior Property Renovation Specialists Covering All of Birmingham.">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+        <script src="https://kit.fontawesome.com/b15a8855ee.js" crossorigin="anonymous"></script>
+        <script src="../Windows/windows.js"></script>
+        <link rel="stylesheet" href="../Windows/windows.css?php echo time(); ?>">
+    </head>
+    <body class="container-fluid p-0">
+        <div id="loading">
+            <img src="../Images/loading.gif" alt="">
+        </div>
+        <header class="row m-0 sticky-top">
+            <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #62abf3;">
+                <a class="navbar-brand ms-2" href="../Windows/windows.php">Sparkling Windows</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navigation">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="../Windows/windows.php">Home</a>
+                        </li>
+                        <li class="nav-item">   
+                            <a class="nav-link" href="../Windows/about.php">About Us</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" data-toggle="dropdown">Services</a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="../Windows/wclean.php">Window Cleaning</a>
+                                <a class="dropdown-item" href="../Windows/gclean.php">Gutter Cleaning</a>
+                                <a class="dropdown-item" href="../Windows/patio.php">Patio Cleaning</a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="../Windows/price.php">Get A Quote</a>
+                            </div>
+                        </li>
+                    </ul>
+                    <form class="form-inline ms-auto my-lg-0">
+                        <div class="input-group">
+                            <input class="form-control me-sm-2" type="search" placeholder="Search" >
+                            <button id="searchBut" class="btn btn-outline-success me-2 my-sm-0" type="submit">Search</button>
+                        </div>
+                    </form>
+                </div>
+            </nav>
+        </header>
+        <main class="row m-0">
+            <div class="col-12 p-0" style="width: 100%;">
+                <div class="row my-1">
+                    <div class="col-12 col-sm-4" style="z-index: 2;">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d2424.0168763907845!2d-1.8413097703946!3d52.58738811280315!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNTLCsDM1JzEzLjQiTiAxwrA1MCcyMS40Ilc!5e0!3m2!1sen!2sim!4v1729131363415!5m2!1sen!2sim" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                        <div class="col-12 col-sm-8 text-center" style="z-index: 2;">
+                        <h3>Find Us:</h1>
+                        </br>
+                        <h2>Sparkling Windows</h2>
+                        </br>
+                        <h3>33 All Saints Drive</h3>
+                        <h3>Birmingham</h3>
+                        <h3>United Kingdom</h3>
+                        <h3>B74 4AG</h3>
+                        </br>
+                        <p><a href="tel:+447624315231">Phone</a></p>
+                        <p><a href="mailto:tylerannis55@gmail.com">Email</a></p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 col-sm-8 order-2 order-sm-1 text-center">
+                        <h1>About Us</h1>
+                        <p>At Sparkling Windows, we are dedicated to providing a full range of high-quality exterior cleaning services that help keep your home or business looking its absolute best. While we specialize in window cleaning—ensuring a streak-free, crystal-clear shine that enhances the view and appearance of your property—we also offer a variety of additional services to meet all your exterior cleaning needs. Our gutter cleaning service ensures your gutters are free of debris, preventing clogs and water damage, while our patio cleaning services use advanced techniques to restore the beauty of outdoor spaces by removing dirt, grime, moss, and other buildup. Our professional team is fully trained and equipped with industry-leading tools, ensuring that every job is handled with precision, care, and safety. We take pride in our eco-friendly approach, using non-toxic cleaning solutions to deliver spotless results without harming the environment or your property. Whether its a routine maintenance plan for your windows and gutters or a one-time deep clean for your patio or driveway, we provide flexible scheduling to accommodate your busy lifestyle.</p>
+                    </div>
+                    <div class="col-12 col-sm-4 order-1 order-sm-2"> 
+                        <img src="../Images/castle.jpg" alt="" style="width: 100%; height: 100%;">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 text-center">
+                        <h1>Company Aims</h1>
+                    </div>
+                    <div class="col-12 col-sm-4" style="border: 1px, solid, black;">
+                        <p><strong>Unmatched Quality</strong></br></br>We strive to provide the highest quality cleaning services, ensuring that every window, gutter, and patio we clean is left spotless and well-maintained. We use state-of-the-art equipment and eco-friendly products to achieve a flawless finish, paying attention to the smallest details to meet and exceed customer expectations. Our goal is to leave every property looking its absolute best, enhancing both appearance and value.</p>
+                    </div>
+                    <div class="col-12 col-sm-4" style="border: 1px, solid, black;">
+                        <p><strong>Ensure Reliability and Trust</strong></br></br>At Sparkling Windows, we know that trust is key in any client relationship. Thats why we prioritize punctuality, professionalism, and transparency in all our operations. From clear communication during the scheduling process to delivering consistent, reliable service, we ensure that our clients can depend on us for timely and efficient cleaning solutions. Whether it's a routine visit or an emergency gutter cleaning, we treat every job with the same level of care and commitment.</p>
+                    </div>
+                    <div class="col-12 col-sm-4" style="border: 1px, solid, black;">
+                        <p><strong>Provide Exceptional Customer Care</strong></br></br>Our clients satisfaction is our top priority, and we believe in creating a seamless and pleasant experience from start to finish. We listen to our clients needs and tailor our services accordingly, offering flexible scheduling and customized cleaning plans to suit each propertys specific requirements. We go above and beyond to address any concerns and provide follow-up services to guarantee our clients are completely satisfied. At Sparkling Windows, we arent happy until our clients are delighted with the results.</p>
+                    </div>
+                </div>
+            </div>
+        </main>
+        <footer class="row d-flex justify-content-center align-items-center">
+            <div class="col-7 col-md-3" style="overflow: hidden;">
+                <img class="ms-2" src="../Images/aurelia.jpg" alt="" style="width: 100%; height:40vh; border-radius:1em">
+            </div>
+            <div class="col-12 col-sm-6 col-md-5 text-center order-2 order-md-1">
+                <ul class="p-0">
+                    <li>
+                        <a href="../Windows/windows.php">Home</a>
+                    </li>
+                    </br>
+                    <li>
+                        <a href="../Windows/about.php">About Us</a>
+                    </li>
+                    </br>
+                    <li>
+                        <a href="../Windows/wclean.php">Window Cleaning</a>
+                    </li>
+                    </br>
+                    <li>
+                        <a href="../Windows/gclean.php">Gutter Cleaning</a>
+                    </li>
+                    </br>
+                    <li>
+                        <a href="../Windows/patio.php">Patio Cleaning</a>
+                    </li>
+                    </br>
+                    <li>
+                        <a href="../Windows/price.php">Get A Quote</a>
+                    </li>
+                    <hr>
+                </ul>
+            </div>
+            <div class="col-12 col-sm-6 col-md-4 text-center order-1 order-md-2">
+                <h3>Sparkling Windows</h2>
+                </br>
+                <p>33 All Saints Drive</p>
+                <p>Birmingham</p>
+                <p>United Kingdom</p>
+                <p>B74 4AG</p>
+                </br>
+                <p><a href="tel:+447624315231">Phone</a></p>
+                <p><a href="mailto:tylerannis55@gmail.com">Email</a></p>
+                <hr>
+            </div>
+        </footer>
+    </body>
+</html>
